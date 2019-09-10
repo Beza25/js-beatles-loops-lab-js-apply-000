135b33b6fcbf67eba743a6e35ec6eebc46712650
Beza Sirak <sirak.bezawit@gmail.com>
2019-09-10 18:33:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // add solution here
+
+function theBeatlesPlay(musicians, instruments){
+  var newArr = [];
+  for (var i; i<musicians.length; i++){
+     newArr[i] += console.log(musicians[i] + " plays " + instruments[i]);
+    }
+  return newArr;
+}
+
+theBeatlesPlay(musicians, instruments);
+
+
+function johnLennonFacts(facts){
+  var arr = [];
+  var i = 0;
+  while(i< facts.length){
+    arr += console.log(facts[i] + "!!!");
+    i++;
+  }
+}
+
+function iLoveTheBeatles (number){
+  var newArry = [];
+  var i = 0; 
+  
+  do{
+    newArry[i] = "I love the Beatles!"
+    i++
+    
+  }while(number < 15)
+  number++
+
+  return newArry
+}
+  // do {
+  //   newArry[i] = "I love the Beatles!";
+  //   i++
+  // }while ( number < 15){
+  // // newArry[i] = "I love the Beatles!";
+  //   number++
+ 
+  // }
+  // return newArry;
+  // }
+  
+
